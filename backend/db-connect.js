d29a6470a53eb0df9a81325539a2361543713636
Laila Beqiri <lailabeqiri@libero.it>
2024-03-19 11:20:48 +0100

    server is running DB is connected

---
+++ backend/db-connect.js
@@ -0,0 +1,20 @@
+import "./config.js";
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+
+  try {
+    await mongoose.connect(process.env.URI);
+    console.log(`DB connected `);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+export default connectDb();
+
+
+
+
+
