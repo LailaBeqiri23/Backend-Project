92261fa3983b1b34a86408234d9dc4af8165f3e5
Laila Beqiri <lailabeqiri@libero.it>
2024-04-09 09:17:04 +0200

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+const handelUsernameChange = (e) => {
+  setUsername(e.target.value)
+}
+
+const handelEmailChange = (e) => {
+  setEmail(e.target.value)
+}
+
+const handelPassswordChange = (e) => {
+  setPassword(e.target.value)
+}
+
+  const handleRegister = async (e) => {
+    e.preventDefault()
+    const response = await fetch('http://localhost:5000/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      })
+    })
+    console.log(response)
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <form onSubmit={handleRegister}>
+      <input type="text" name="username" placeholder="username" onChange={handelUsernameChange}/>
+      <input type="email" name="email" placeholder="Email" onChange={handelEmailChange}/>
+      <input type="password" name="password" placeholder="Password" onChange={handelPassswordChange}/>
+      <input type="submit" value="Register" />
+     </form>
     </>
   )
 }
